21194f8c8c21fbcd4a24ded75e4ac1771f9d764b
expiriensu <gundari_dan@bk.ru>
2021-07-27 23:17:47 +0700

    changed index.php, add flex clear intotsh600, changed products.php

--- app/index.php
+++ app/index.php
@@ -1,24 +1,4 @@
 <?php include 'template/head.php';?>
-    <ul>
-      <li>
-        <a href="about.php">
-          <h1>О нас</h1>
-        </a>
-      </li>
-      <li>
-        <a href="products.php">
-          <h1>Продукция</h1>
-        </a>
-      </li>
-      <li>
-        <a href="development.php">
-          <h1>Разработки</h1>
-        </a>
-      </li>
-      <li>
-        <a href="contacts.php">
-          <h1>Контакты</h1>
-        </a>
-      </li>
-    </ul>
+    <h1>РЕССА</h1>
+    <h2>Разработка и производство гусеничных тракторов</h2>
 <?php include 'template/foot.php';?>
